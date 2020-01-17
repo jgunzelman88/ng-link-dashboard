4e9fbd291702f7caf2c3fa8c5127e0358fea2829
jeffrey.p.gunzelman <jeffrey.gunzelman@phoenix-opsgroup.com>
2020-01-16 20:41:02 -0500

    added material lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config.ts
@@ -0,0 +1,10 @@
+export class Link{
+    public icon: String;
+    public label: String;
+    public link: String;
+}
+export class DashboardConfig{
+    public title : String;
+    public logo : String;
+    public links : Array<Link>;
+}
